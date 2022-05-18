1557125c156afb84f5964f510d9a194a8f22cee3
imj22 <98693440+imj22@users.noreply.github.com>
2022-05-18 15:46:27 +0200

    commit tree 7d0bd7ef73dccfcfb9d4f9f692362c9736f9244b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,39 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    if self.chars.pop == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    self.chars.pop == "?" ? true : false
   end
 
   def exclamation?
-
+    self.chars.pop == "!" ? true : false
   end
 
   def count_sentences
-
+    count = 0
+    
+    self.chars.count("!") + self.chars.count("?") + self.chars.count(".") 
   end
 end
+
+#  "Hi, my name is frog.".sentence?
+
+
+# "hi my name is frog without period".sentence?
+
+
+# true_test = "is this !".exclamation?
+# puts "should be true: #{true_test}"
+# false_test =  "this is not frog".exclamation?
+# puts "should be false: #{false_test}"
+
+test = "hi !! hello ! nope...".count_sentences
+puts test
